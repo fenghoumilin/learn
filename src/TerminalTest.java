94a7be8981f0b431b34072bf21c2668391faab37
刘维 <liuwei@liuweideMBP.lan>
2018-08-28 19:19:21 +0800

    害怕

--- src/TerminalTest.java
+++ src/TerminalTest.java
@@ -1,38 +1,12 @@
+import java.util.Date;
 
 public class TerminalTest {
 
     public static void main(String[] args) {
 
-        String deviceModel = "iPhonessss";
+        Date date = new Date();
+        System.out.println(date);
 
-        System.out.println(getDeviceType(deviceModel));
+
     }
-
-
-    public static String getDeviceType(String deviceModel) {
-
-        deviceModel = deviceModel.trim();
-        //判断是否有 | 来区分ios 和 android
-        int index = deviceModel.lastIndexOf("|");
-        System.out.println(index);
-        if (index > 0) {
-            //android
-            String sub = deviceModel.substring(index+1);
-            if ("phone".equals(sub)) {
-                return "phone";
-            }else {
-                return "pad";
-            }
-
-        }else {
-            //ios
-            if (deviceModel.contains("iPhone") ){
-                return "phone";
-            }else {
-                return "pad";
-            }
-        }
-    }
-
-
 }
